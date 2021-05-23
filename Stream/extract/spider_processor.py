94a48a973da5da4040de63a9cd28d9b11d30c79e
stormbroken <181250184@smail.nju.edu.cn>
2021-05-23 15:50:07 +0800

    feat: 简单的爬虫机器人统计

--- Stream/extract/spider_processor.py
+++ Stream/extract/spider_processor.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
-
-import pandas as pd
 import os
+import pandas as pd
 from tqdm import tqdm
 
 '''
@@ -17,5 +16,73 @@ class SpiderProcessor:
         if not os.path.exists(target):
             os.mkdir(target)
 
-    def run(self):
-        print("Do Analyze")
+    def near_time(self, number):
+        """
+        调整到最近的一分钟
+        :param number:
+        :return:
+        """
+        n = number % 60
+        if (n > 30):
+            number = number - n + 60
+        else:
+            number -= n
+        return number
+
+    def group(self, query):
+        """
+        根据 query 进行 group by 后进行统计
+        :param filename: 绝对路径
+        :param query:
+        :return:
+        """
+        df = pd.read_csv(self.source, encoding='utf-8', header=0)
+        df["TIME"] = df["TIME"].apply(self.near_time)
+        gb = df.groupby(query)
+        result = gb.size().to_frame(name="counts").reset_index()
+        filename = "spider_" + "_".join(query).lower() + ".csv"
+        result.to_csv(os.path.join(self.target, filename), index=False, mode="w")
+
+    def filter(self, filename):
+        """
+        没有抢到过的用户被过滤掉
+        :param filename:
+        :return:
+        """
+        df = pd.read_csv(filename, encoding='utf-8', header=0)
+        result = df[df["counts"] > 1]
+        filename = filename.replace(".csv", "_filter.csv")
+        result.to_csv(os.path.join(self.target, filename), index=False, mode="w")
+
+    def analysis(self, filename, query=["mean"]):
+        df = pd.read_csv(filename, encoding='utf-8', header=0)
+        frame = df.describe()
+        if(type(query) == int):
+            condition = query
+        else:
+            condition = frame["counts"][query[0]]
+        result = df[df["counts"] >= condition]
+        filename = filename.replace(".csv", "_result.csv")
+        result.to_csv(os.path.join(self.target, filename), index=False, mode="w")
+
+    def run(self, query, analysis_query):
+        """
+        :return:
+        """
+        print("Group")
+        self.group(query)
+        filename = os.path.join(self.target, "spider_" + "_".join(query).lower() + ".csv")
+        print("filter")
+        self.filter(filename)
+        filename = filename.replace(".csv", "_filter.csv")
+        print("analysis")
+        self.analysis(filename, analysis_query)
+
+
+if __name__ == '__main__':
+    s = SpiderProcessor("../category/itemgetDetail_new.csv", "../spider")
+    queries = {
+        "[\"userId\", \"TIME\", \"itemId\", \"categoryId\"]": ["mean"],
+    }
+    for query in tqdm(queries):
+        s.run(eval(query), queries[query])
